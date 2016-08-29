bd13f6265e04271da3b96b77512881db7377c305
meganrm <meganrm@gmail.com>
2016-08-29 11:50:06 -0700

    set up page controllers

--- class-11-spa-routing/lecture/demo/app.js
+++ class-11-spa-routing/lecture/demo/app.js
@@ -1,1 +1,14 @@
 
+page();
+page('/', homeController); //invoking page.js
+page('/contact', contactController);
+
+function homeController(){
+  document.querySelector('p')
+    .textContent = 'Index page was generated!';
+}
+
+function contactController(){
+  document.querySelector('p')
+    .textContent = 'Contact page was generated';
+}
